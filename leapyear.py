e9a6824a73e4dccf84800312644c2c0cd942fe60
labib1235 <hasan2008khalid@gamil.com>
2021-06-29 00:22:30 +0600

    Leap year founder

---
+++ leapyear.py
@@ -0,0 +1,10 @@
+print("Input the year: ")
+year = int(input())
+if year % 400 == 0:
+        print("Yes")
+elif year % 100 == 0:
+        print("No")
+elif year % 4 == 0: 
+        print("Yes")
+else:
+    print("No") 
