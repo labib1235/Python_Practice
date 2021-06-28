e690c9e996a91e64da248fc83067b42c755ed5a2
labib1235 <hasan2008khalid@gamil.com>
2021-06-29 00:18:54 +0600

    k

--- filter.py
+++
@@ -1,9 +0,0 @@
-my_list = [1, 2, 3, 4, 5, 6, 7]
-def is_even(x):
-    if (x % 2) == 0:
-        return True
-    else:
-        return False
-new_list = filter(is_even, my_list)
-print(new_list)
-print(list(new_list))
